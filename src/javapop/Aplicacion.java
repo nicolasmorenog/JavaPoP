9957072b9ecdcb9bbdeed27df8308d9cff8aa421
FLAYS <61749652+nicolasmorenog@users.noreply.github.com>
2020-06-16 04:59:32 +0200

    Update Aplicacion.java

--- src/javapop/Aplicacion.java
+++ src/javapop/Aplicacion.java
@@ -1,4 +1,3 @@
-
 package javapop;
 
 import java.io.BufferedReader;
@@ -12,18 +11,26 @@ import static javapop.Variables.listaProductos;
  *
  * @author Jesús Palomino Abreu
  * @author Nicolás Moreno González
- * 
+ *
  * @version v2.0 06/2019
  */
 public class Aplicacion implements Serializable {
 
+    /**
+     * método que devuelve el tipo del correo introducido
+     *
+     * @return obj. retorna un objeto vacío si no se ha encontrado ningún correo
+     * que coincida
+     * @param correo. correo tipo (String) a buscar
+     */
     public static Object buscarCorreo(String correo) {
 
-        /*for (ClienteProfesional clienteProfesional : listaClientesProfesionales) {
-            if (correo.equals(clienteProfesional.getCorreo())) {
-                return clienteProfesional;
-            }
-        }*/
+        /**
+         * recorre la lista de clientes hasta encontrar el correo del cliente o
+         * hasta que llegue al final de la lista
+         *
+         * @return cliente. cliente asociado al correo encontrado
+         */
         for (Cliente cliente : listaClientes) {
             if (correo.equals(cliente.getCorreo())) {
                 return cliente;
@@ -31,6 +38,11 @@ public class Aplicacion implements Serializable {
         }
 
         Admin admin = new Admin();
+        /**
+         * mira si el correo coincide con el correo del administrador
+         *
+         * @return admin. admin asociado al correo encontrado
+         */
         if (correo.equals(admin.getCorreo())) {
             return admin;
         }
@@ -39,18 +51,24 @@ public class Aplicacion implements Serializable {
         return obj;
     }
 
+    /**
+     * método que devuelve el index del correo introducido
+     *
+     * @param correo. correo tipo (String) a buscar
+     * @return -1. retorna -1 si no se ha encontrado ningún correo que coincida
+     */
     public static int buscarCorreoIndex(String correo) {
 
-        /*for (ClienteProfesional clienteProfesional : listaClientesProfesionales) {
-            if (correo.equals(clienteProfesional.getCorreo())) {
-                return clienteProfesional;
-            }
-        }*/
         int c = 0;
-
+        /**
+         * recorre la lista de clientes hasta encontrar el correo del cliente o
+         * hasta que llegue al final de la lista
+         *
+         * @return c. index del correo encontrado
+         */
         for (Cliente cliente : listaClientes) {
             if (correo.equals(cliente.getCorreo())) {
-                
+
                 return c;
             }
             c++;
@@ -65,16 +83,36 @@ public class Aplicacion implements Serializable {
 
     }
 
+    /**
+     * método para saber si el nuevo correo es apto para registro o no
+     *
+     * @param correo
+     * @return boolean. devuelve si el correo es apto para registro o no
+     */
     public static boolean correoApto(String correo) {
         Object user = buscarCorreo(correo);
-        if (user instanceof Cliente || user instanceof ClienteProfesional || user instanceof Admin) {
+        /**
+         * llama al método buscarCorreo() y mira si el correo es instancia de
+         * Cliente o de Admin
+         */
+        if (user instanceof Cliente || user instanceof Admin) {
             return false;
         }
         return true;
     }
 
+    /**
+     *
+     * @param titulo
+     * @return encontrados. devuelve si no ha encontrado ningún título que
+     * coincida
+     */
     public static ArrayList<Producto> buscarTitulo(String titulo) {
         ArrayList<Producto> encontrados = new ArrayList<Producto>();
+        /**
+         * recorre la lista de productos hasta encontrar una coincidencia con el
+         * titulo del producto o hasta que llegue al final de la lista
+         */
         for (Producto producto : listaProductos) {
             if (titulo.equals(producto.getTitulo())) {
                 encontrados.add(producto);
@@ -85,15 +123,32 @@ public class Aplicacion implements Serializable {
 
     }
 
+    /**
+     *
+     * @return Object
+     * @exception e. lanza la excepción e si no entra en ninguna de las
+     * condiciones
+     */
     public static Object login() {
         BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
         try {
+            /**
+             * @return user. devuelve el usuario si no entra en ninguna de las
+             * condiciones
+             */
+            /**
+             * se pide al usuario que introduzca su corre y contraseña
+             */
             System.out.println("Introduce el correo: ");
             String correo = entrada.readLine();
             System.out.println("Introduce la clave: ");
             String contraseña = entrada.readLine();
 
             Object user = buscarCorreo(correo);
+            /**
+             *
+             */
             if (user instanceof Cliente) {
                 Cliente cliente = (Cliente) user;
                 if (contraseña.equals(cliente.getClave())) {
@@ -118,10 +173,15 @@ public class Aplicacion implements Serializable {
                     System.out.println("La contraseña es incorrecta");
                     user = new Object();
                 }
+                /**
+                 * @return user. devuelve el usuario si el correo no está
+                 * asociado a ninguna cuenta de usuario
+                 */
             } else {
                 System.out.println("El correo indicado no esta asociado a ninguna cuenta de usuario, pruebe a registrarse.");
                 return user;
             }
+
             return user;
         } catch (Exception e) {
             System.out.println("Ha surgido un error: " + e.toString());
